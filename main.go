8b8998b261f34edad3436e82b3dbd33613307930
Yannic Rieger <riegeryannic@gmail.com>
2018-08-29 18:47:52 +0200

    Start work

--- main.go
+++ main.go
@@ -1,5 +1,20 @@
 package main
 
+import (
+	"github.com/gorilla/mux"
+	"log"
+	"net/http"
+)
+
 func main() {
-	
+	log.SetPrefix("[PlayerResolver]")
+	log.Print("Starting player resolver...")
+	router := mux.NewRouter()
+	router.HandleFunc("/uuid", GetUuidFromName).Methods("GET")
+	http.ListenAndServe(":8080", router)
+}
+
+
+func GetUuidFromName(w http.ResponseWriter, r *http.Request) {
+
 }
